cf918041313d60e6d0e47f011157709baa87a644
Papis-prog <abdoulayediakhite869@gmail.com>
2025-11-05 23:23:58 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -139,7 +139,7 @@ app.post(
 // --- SERVIR LE FRONTEND REACT ---
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
